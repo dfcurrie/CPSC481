30fc668cdfb02325f971faf70f5c7d56b68dc57f
Rayce Rossum <Rayce.Rossum@gmail.com>
2016-04-03 18:00:28 -0600

    Add new stats page

--- SkiAndBoardApp/StatisticsPage.xaml.cs
+++ SkiAndBoardApp/StatisticsPage.xaml.cs
@@ -20,6 +20,150 @@ namespace SkiAndBoardApp {
     public partial class StatisticsPage : Page {
         public StatisticsPage() {
             InitializeComponent();
+
+            Friend1_pic.Visibility = System.Windows.Visibility.Visible;
+            Friend1_numRuns.Visibility = System.Windows.Visibility.Visible;
+            Friend1_dist.Visibility = System.Windows.Visibility.Visible;
+            Friend1_airTime.Visibility = System.Windows.Visibility.Visible;
+            Friend1_elev.Visibility = System.Windows.Visibility.Visible;
+            Friend1_vertical.Visibility = System.Windows.Visibility.Visible;
+            Friend1_maxSpeed.Visibility = System.Windows.Visibility.Visible;
+            Friend1_cal.Visibility = System.Windows.Visibility.Visible;
+            Friend1_avgHR.Visibility = System.Windows.Visibility.Visible;
+            Friend1_time.Visibility = System.Windows.Visibility.Visible;
+
+            Friend2_pic.Visibility = System.Windows.Visibility.Hidden;
+            Friend2_numRuns.Visibility = System.Windows.Visibility.Hidden;
+            Friend2_dist.Visibility = System.Windows.Visibility.Hidden;
+            Friend2_airTime.Visibility = System.Windows.Visibility.Hidden;
+            Friend2_elev.Visibility = System.Windows.Visibility.Hidden;
+            Friend2_vertical.Visibility = System.Windows.Visibility.Hidden;
+            Friend2_maxSpeed.Visibility = System.Windows.Visibility.Hidden;
+            Friend2_cal.Visibility = System.Windows.Visibility.Hidden;
+            Friend2_avgHR.Visibility = System.Windows.Visibility.Hidden;
+            Friend2_time.Visibility = System.Windows.Visibility.Hidden;
+
+            Friend3_pic.Visibility = System.Windows.Visibility.Hidden;
+            Friend3_numRuns.Visibility = System.Windows.Visibility.Hidden;
+            Friend3_dist.Visibility = System.Windows.Visibility.Hidden;
+            Friend3_airTime.Visibility = System.Windows.Visibility.Hidden;
+            Friend3_elev.Visibility = System.Windows.Visibility.Hidden;
+            Friend3_vertical.Visibility = System.Windows.Visibility.Hidden;
+            Friend3_maxSpeed.Visibility = System.Windows.Visibility.Hidden;
+            Friend3_cal.Visibility = System.Windows.Visibility.Hidden;
+            Friend3_avgHR.Visibility = System.Windows.Visibility.Hidden;
+            Friend3_time.Visibility = System.Windows.Visibility.Hidden;
+
         }
+
+     
+        private void Friend1_Selected(object sender, RoutedEventArgs e)
+        {
+            Friend1_pic.Visibility = System.Windows.Visibility.Visible;
+            Friend1_numRuns.Visibility = System.Windows.Visibility.Visible;
+            Friend1_dist.Visibility = System.Windows.Visibility.Visible;
+            Friend1_airTime.Visibility = System.Windows.Visibility.Visible;
+            Friend1_elev.Visibility = System.Windows.Visibility.Visible;
+            Friend1_vertical.Visibility = System.Windows.Visibility.Visible;
+            Friend1_maxSpeed.Visibility = System.Windows.Visibility.Visible;
+            Friend1_cal.Visibility = System.Windows.Visibility.Visible;
+            Friend1_avgHR.Visibility = System.Windows.Visibility.Visible;
+            Friend1_time.Visibility = System.Windows.Visibility.Visible;
+
+            Friend2_pic.Visibility = System.Windows.Visibility.Hidden;
+            Friend2_numRuns.Visibility = System.Windows.Visibility.Hidden;
+            Friend2_dist.Visibility = System.Windows.Visibility.Hidden;
+            Friend2_airTime.Visibility = System.Windows.Visibility.Hidden;
+            Friend2_elev.Visibility = System.Windows.Visibility.Hidden;
+            Friend2_vertical.Visibility = System.Windows.Visibility.Hidden;
+            Friend2_maxSpeed.Visibility = System.Windows.Visibility.Hidden;
+            Friend2_cal.Visibility = System.Windows.Visibility.Hidden;
+            Friend2_avgHR.Visibility = System.Windows.Visibility.Hidden;
+            Friend2_time.Visibility = System.Windows.Visibility.Hidden;
+
+            Friend3_pic.Visibility = System.Windows.Visibility.Hidden;
+            Friend3_numRuns.Visibility = System.Windows.Visibility.Hidden;
+            Friend3_dist.Visibility = System.Windows.Visibility.Hidden;
+            Friend3_airTime.Visibility = System.Windows.Visibility.Hidden;
+            Friend3_elev.Visibility = System.Windows.Visibility.Hidden;
+            Friend3_vertical.Visibility = System.Windows.Visibility.Hidden;
+            Friend3_maxSpeed.Visibility = System.Windows.Visibility.Hidden;
+            Friend3_cal.Visibility = System.Windows.Visibility.Hidden;
+            Friend3_avgHR.Visibility = System.Windows.Visibility.Hidden;
+            Friend3_time.Visibility = System.Windows.Visibility.Hidden;
+        }
+
+        private void Friend2_Selected(object sender, RoutedEventArgs e)
+        {
+            Friend2_pic.Visibility = System.Windows.Visibility.Visible;
+            Friend2_numRuns.Visibility = System.Windows.Visibility.Visible;
+            Friend2_dist.Visibility = System.Windows.Visibility.Visible;
+            Friend2_airTime.Visibility = System.Windows.Visibility.Visible;
+            Friend2_elev.Visibility = System.Windows.Visibility.Visible;
+            Friend2_vertical.Visibility = System.Windows.Visibility.Visible;
+            Friend2_maxSpeed.Visibility = System.Windows.Visibility.Visible;
+            Friend2_cal.Visibility = System.Windows.Visibility.Visible;
+            Friend2_avgHR.Visibility = System.Windows.Visibility.Visible;
+            Friend2_time.Visibility = System.Windows.Visibility.Visible;
+
+            Friend1_pic.Visibility = System.Windows.Visibility.Hidden;
+            Friend1_numRuns.Visibility = System.Windows.Visibility.Hidden;
+            Friend1_dist.Visibility = System.Windows.Visibility.Hidden;
+            Friend1_airTime.Visibility = System.Windows.Visibility.Hidden;
+            Friend1_elev.Visibility = System.Windows.Visibility.Hidden;
+            Friend1_vertical.Visibility = System.Windows.Visibility.Hidden;
+            Friend1_maxSpeed.Visibility = System.Windows.Visibility.Hidden;
+            Friend1_cal.Visibility = System.Windows.Visibility.Hidden;
+            Friend1_avgHR.Visibility = System.Windows.Visibility.Hidden;
+            Friend1_time.Visibility = System.Windows.Visibility.Hidden;
+
+            Friend3_pic.Visibility = System.Windows.Visibility.Hidden;
+            Friend3_numRuns.Visibility = System.Windows.Visibility.Hidden;
+            Friend3_dist.Visibility = System.Windows.Visibility.Hidden;
+            Friend3_airTime.Visibility = System.Windows.Visibility.Hidden;
+            Friend3_elev.Visibility = System.Windows.Visibility.Hidden;
+            Friend3_vertical.Visibility = System.Windows.Visibility.Hidden;
+            Friend3_maxSpeed.Visibility = System.Windows.Visibility.Hidden;
+            Friend3_cal.Visibility = System.Windows.Visibility.Hidden;
+            Friend3_avgHR.Visibility = System.Windows.Visibility.Hidden;
+            Friend3_time.Visibility = System.Windows.Visibility.Hidden;
+        }
+
+        private void Friend3_Selected(object sender, RoutedEventArgs e)
+        {
+            Friend3_pic.Visibility = System.Windows.Visibility.Visible;
+            Friend3_numRuns.Visibility = System.Windows.Visibility.Visible;
+            Friend3_dist.Visibility = System.Windows.Visibility.Visible;
+            Friend3_airTime.Visibility = System.Windows.Visibility.Visible;
+            Friend3_elev.Visibility = System.Windows.Visibility.Visible;
+            Friend3_vertical.Visibility = System.Windows.Visibility.Visible;
+            Friend3_maxSpeed.Visibility = System.Windows.Visibility.Visible;
+            Friend3_cal.Visibility = System.Windows.Visibility.Visible;
+            Friend3_avgHR.Visibility = System.Windows.Visibility.Visible;
+            Friend3_time.Visibility = System.Windows.Visibility.Visible;
+
+            Friend1_pic.Visibility = System.Windows.Visibility.Hidden;
+            Friend1_numRuns.Visibility = System.Windows.Visibility.Hidden;
+            Friend1_dist.Visibility = System.Windows.Visibility.Hidden;
+            Friend1_airTime.Visibility = System.Windows.Visibility.Hidden;
+            Friend1_elev.Visibility = System.Windows.Visibility.Hidden;
+            Friend1_vertical.Visibility = System.Windows.Visibility.Hidden;
+            Friend1_maxSpeed.Visibility = System.Windows.Visibility.Hidden;
+            Friend1_cal.Visibility = System.Windows.Visibility.Hidden;
+            Friend1_avgHR.Visibility = System.Windows.Visibility.Hidden;
+            Friend1_time.Visibility = System.Windows.Visibility.Hidden;
+
+            Friend2_pic.Visibility = System.Windows.Visibility.Hidden;
+            Friend2_numRuns.Visibility = System.Windows.Visibility.Hidden;
+            Friend2_dist.Visibility = System.Windows.Visibility.Hidden;
+            Friend2_airTime.Visibility = System.Windows.Visibility.Hidden;
+            Friend2_elev.Visibility = System.Windows.Visibility.Hidden;
+            Friend2_vertical.Visibility = System.Windows.Visibility.Hidden;
+            Friend2_maxSpeed.Visibility = System.Windows.Visibility.Hidden;
+            Friend2_cal.Visibility = System.Windows.Visibility.Hidden;
+            Friend2_avgHR.Visibility = System.Windows.Visibility.Hidden;
+            Friend2_time.Visibility = System.Windows.Visibility.Hidden;
+        }
+
     }
 }
